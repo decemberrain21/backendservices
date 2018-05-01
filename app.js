949a3e2e2dc129df0dfbeb22ca25ddbab29e0736
nyeinei <decemberrain21@gmail.com>
2018-05-01 23:21:27 +0800

    changed app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+const http = require('http');
+
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World\n');
+});
+
+
  
 server.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
